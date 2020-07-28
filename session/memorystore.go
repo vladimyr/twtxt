vladimyr/twtxt

commit 150690c41a2a098fd161720962ef2dce243903b9

Refactor session handling to not use empty interfaces

session/memorystore.go
@@ -27,7 +27,7 @@ func NewMemoryStore(sessionDuration time.Duration) *MemoryStore {
 //Store interface implementation
 
 //Save associates the provided state data with the provided session id in the store.
-func (ms *MemoryStore) Save(sid SessionID, state interface{}) error {
+func (ms *MemoryStore) Save(sid SessionID, state SessionData) error {
 	j, err := json.Marshal(state)
 	if nil != err {
 		return err
@@ -39,7 +39,7 @@ func (ms *MemoryStore) Save(sid SessionID, state interface{}) error {
 //Get retrieves the previously saved state data for the session id,
 //and populates the `data` parameter with it. This will also
 //reset the data's time to live in the store.
-func (ms *MemoryStore) Get(sid SessionID, state interface{}) error {
+func (ms *MemoryStore) Get(sid SessionID, state SessionData) error {
 	j, found := ms.entries.Get(sid.String())
 	if !found {
 		return ErrStateNotFound

session/manager.go
@@ -9,13 +9,13 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Data ...
-type Data map[string]string
+// SessionData ...
+type SessionData map[string]string
 
 // Session ...
 type Session struct {
 	sid  SessionID
-	data Data
+	data SessionData
 
 	store Store
 }
@@ -39,9 +39,9 @@ func (s *Session) Delete(key string) {
 
 // NewSession ...
 func NewSession(sid SessionID, store Store) *Session {
-	data := make(Data)
+	data := make(SessionData)
 
-	store.Get(sid, &data)
+	store.Get(sid, data)
 
 	return &Session{sid, data, store}
 }

session/store.go
@@ -21,12 +21,12 @@ var ErrStateNotFound = errors.New("your session has expired")
 //or more typically in a shared key/value server store like redis.
 type Store interface {
 	//Save associates the provided `state`` data with the provided `sid` in the store.
-	Save(sid SessionID, state interface{}) error
+	Save(sid SessionID, state SessionData) error
 
 	//Get retrieves the previously saved state data for the session id,
 	//and populates the `state` parameter with it. This will also
 	//reset the data's time to live in the store.
-	Get(sid SessionID, state interface{}) error
+	Get(sid SessionID, state SessionData) error
 
 	//Delete deletes all state data associated with the session id from the store.
 	Delete(sid SessionID) error

commit 58c3c782aa7dcbbfc8473b9a911b1d15b99ac5b5

Fix session unmarshalling caused by 150690c

session/memorystore.go
@@ -44,7 +44,7 @@ func (ms *MemoryStore) Get(sid SessionID, state SessionData) error {
 	if !found {
 		return ErrStateNotFound
 	}
-	return json.Unmarshal(j.([]byte), state)
+	return json.Unmarshal(j.([]byte), &state)
 }
 
 //Delete deletes all state data associated with the session id from the store.