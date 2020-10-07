33768de3e0c5ef71a871d1d02930259766795e2e
James Mills <prologic@shortcircuit.net.au>
2020-10-07 22:43:29 +1000

    Revert "Fix a bug with User.Filter()"
    This reverts commit 772517b464fdf5b25bf00e4b711f23af419ee548.

--- internal/models.go
+++ internal/models.go
@@ -369,9 +369,8 @@ func (u *User) Twter() types.Twter {
 	return types.Twter{Nick: u.Username, URL: u.URL}
 }
 
-func (u *User) Filter(twts []interface{}) (filtered []types.Twt) {
-	for _, twtI := range twts {
-		twt := twtI.(types.Twt)
+func (u *User) Filter(twts []types.Twt) (filtered []types.Twt) {
+	for _, twt := range twts {
 		if u.HasMuted(twt.Twter.URL) {
 			continue
 		}
