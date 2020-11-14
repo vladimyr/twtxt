vladimyr/twtxt

commit 15f25c14e40eb73f4ac5c2200ca094909810e1ab

Fix image target to not push the image built immediately (CI does this)

Makefile
@@ -48,7 +48,6 @@ install: build
 
 image:
 	@docker build --build-arg VERSION="$(VERSION)" --build-arg COMMIT="$(COMMIT)" -t prologic/twtxt .
-	@docker push prologic/twtxt
 
 release:
 	@./tools/release.sh

commit 3d3a8204722295ed8e84dc20d298c4f96855f797

Fix image Makefile target that 15f25c1 broke and add PUBLISH=1 variable so we publish new images again

.github/workflows/docker.yml
@@ -19,4 +19,4 @@ jobs:
           DOCKER_PASSWORD: ${{ secrets.DOCKER_PASSWORD }}
         run: |
           echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-          make image
+          make image PUBLISH=1

Makefile
@@ -47,8 +47,14 @@ install: build
 	@go install ./cmd/twt/...
 	@go install ./cmd/twtd/...
 
+ifeq ($(PUBLISH), 1)
 image:
 	@docker build --build-arg VERSION="$(VERSION)" --build-arg COMMIT="$(COMMIT)" -t prologic/twtxt .
+	@docker push prologic/twtxt
+else
+image:
+	@docker build --build-arg VERSION="$(VERSION)" --build-arg COMMIT="$(COMMIT)" -t prologic/twtxt .
+endif
 
 release:
 	@./tools/release.sh